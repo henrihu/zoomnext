ed5bc1cd77ba5956b074fda02ee29229db8cf6f6
super <super@fintech.com>
2023-04-21 17:15:27 -0700

    uninstall react-chat-element module

--- src/pages/message/index.js
+++ src/pages/message/index.js
@@ -4,7 +4,6 @@ import { useSelector, useDispatch } from 'react-redux';
 // Components
 import Meta from '@/components/Meta/index';
 import { Card, Row, Col, Spin, Space, Button } from 'antd';
-import ChatList from './ChatList';
 import UserList from './UserList';
 import MessageList from './MessageList';
 
