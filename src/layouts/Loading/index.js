3f20cde7fcc1189bf07525de9085afed4a760890
super <super@123.com>
2023-05-06 02:23:11 +0900

    fix some issue: header, message

--- src/layouts/Loading/index.js
+++ src/layouts/Loading/index.js
@@ -1,10 +1,10 @@
+import Logo from '@/components/Logo';
 import { Spin } from 'antd';
-import Image from 'next/image';
 
 export default () => {
   return (
     <div className="loading-container">
-      <Image src="/images/login_logo.jpg" width={200} height={100} />
+      <Logo width={200} height={100} />
       <Spin spinning={true} />
     </div>
   );
--- src/pages/helper/payment_settings/index.js
+++ src/pages/helper/payment_settings/index.js
@@ -8,14 +8,6 @@ export default () => {
         description="Zoom Errands"
         label="Payout Settings"
       />
-      <iframe
-        name="I1"
-        id="if1"
-        width="100%"
-        height="100vh"
-        style={{ height: '100vh' }}
-        src="http://10.97.5.121/elog/url/list?flag=dev&url=connect.stripe"
-      ></iframe>
     </>
   );
 };
